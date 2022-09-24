ade625302e9b1975224f11da67dd27394761b81d
derderderdadada <114236014+derderderdadada@users.noreply.github.com>
2022-09-24 11:15:22 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -74,15 +74,15 @@ export const USER_CONFIG = {
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://wangxinleo.cn",
+      openUrl: "https://s.weibo.com/top/summary",
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09"},
-        {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
+        {"type": "生日", "name": "ZYY", "year": "1997", "date": "01-21"},
+        {"type": "节日", "name": "英语四级口试", "year": "2022", "date": "11-19"},
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
-        {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+        //{"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
+        {"type": "节日", "name": "英语四级笔试", "year": "2022", "date": "12-10"},
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
@@ -95,17 +95,15 @@ export const USER_CONFIG = {
       ]
     },
     {
-      name: "老婆1",
+      name: "ZYY",
       id: "oGPQE5yMJi8pt-uvd84AihBlD40Y",
       useTemplateId: "SIJ1x9QPbZqFifYPSiKsuUS2MlC4aN6Qgpu_h8zjTZ0",
       province: "四川",
       city: "宜宾",
       horoscopeDate: '1-21',
       horoscopeDateType: '今日',
-      openUrl: "https://wangxinleo.cn",
-      festivals: [{"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
-        {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"}],
-      customizedDateList: [{"keyword": "love_day", date: "2022-09-08"}]
+      openUrl: "https://s.weibo.com/top/summary",
+      festivals: []
     },
     {
       name: "老婆2",
@@ -148,7 +146,7 @@ export const USER_CONFIG = {
       // 一般都填自己
       name: "自己",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "oGPQE5yMJi8pt-uvd84AihBlD40Y",
+      id: "",
     },
     // 你可以不断按格式往下增加
     // ...
@@ -186,7 +184,7 @@ export const USER_CONFIG = {
    */
   FESTIVALS: [
     // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-    {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09", isShowAge: true},
+    {"type": "*生日", "name": "", "year": "1996", "date": "09-09", isShowAge: true},
     {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
     // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
     {"type": "生日", "name": "李四", "year": "1996", "date": "09-31", isShowAge: true},
@@ -200,7 +198,7 @@ export const USER_CONFIG = {
    * 如果为3, 则仅展示“将要到达” 的3个重要节日提醒，剩下的将被忽略
    * 如果为0, 则默认展示全部
    */
-  FESTIVALS_LIMIT: 4,
+  FESTIVALS_LIMIT: 3,
   
   /** 日期相关 */
   
