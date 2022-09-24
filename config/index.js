91025e1a1963debecaaeb07c1dcf771d5cd3fc91
derderderdadada <114236014+derderderdadada@users.noreply.github.com>
2022-09-24 15:49:35 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -62,7 +62,7 @@ export const USER_CONFIG = {
       // 想要发送的人的名字
       name: "ZYY",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "oGPQE5yMJi8pt-uvd84AihBlD40Y","oGPQE5-VlhSjEjBdE2SLEi-AjSKg"
+      id: "oGPQE5yMJi8pt-uvd84AihBlD40Y",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "oGPQE5yMJi8pt-uvd84AihBlD40Y",
       // 所在省份
@@ -70,7 +70,7 @@ export const USER_CONFIG = {
       // 所在城市
       city: "宜宾",
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '12-27',
+      horoscopeDate: '01-21',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
@@ -95,8 +95,8 @@ export const USER_CONFIG = {
     },
     {
       name: "ZYY",
-      id: "",
-      useTemplateId: "",
+      id: "oGPQE5yMJi8pt-uvd84AihBlD40Y",
+      useTemplateId: "oGPQE5yMJi8pt-uvd84AihBlD40Y",
       province: "四川",
       city: "宜宾",
       horoscopeDate: '1-21',
