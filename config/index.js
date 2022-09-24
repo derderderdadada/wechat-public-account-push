0a168c8467762c2d59e841cc011b088a1292102e
derderderdadada <114236014+derderderdadada@users.noreply.github.com>
2022-09-24 15:46:19 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -62,7 +62,7 @@ export const USER_CONFIG = {
       // 想要发送的人的名字
       name: "ZYY",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "oGPQE5yMJi8pt-uvd84AihBlD40Y","oGPQE5-VlhSjEjBdE2SLEi-AjSKg"
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "oGPQE5yMJi8pt-uvd84AihBlD40Y",
       // 所在省份
@@ -95,8 +95,8 @@ export const USER_CONFIG = {
     },
     {
       name: "ZYY",
-      id: "oGPQE5yMJi8pt-uvd84AihBlD40Y",
-      useTemplateId: "oGPQE5yMJi8pt-uvd84AihBlD40Y",
+      id: "",
+      useTemplateId: "",
       province: "四川",
       city: "宜宾",
       horoscopeDate: '1-21',
