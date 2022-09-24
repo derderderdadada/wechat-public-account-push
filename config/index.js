fe1caaa1a6836d51d579f795e1b4f1c71da98e42
derderderdadada <114236014+derderderdadada@users.noreply.github.com>
2022-09-24 10:07:05 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ export const USER_CONFIG = {
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "9acc2f09eff7669cf9c98269aba853fa",
+  APP_SECRET: "f1f1f72f98b2c9a0b3cde5e2e106ba5e",
   
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -60,11 +60,11 @@ export const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "Z",
+      name: "ZYY",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oGPQE5yMJi8pt-uvd84AihBlD40Y",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "MYSGVrdz30GPcza3LkjED-ZQ1WI_ijd2HkzzrMQUcms",
+      useTemplateId: "IHszGXdnBWvleKCbQiHAoAUrKLnEEaxAhyR0urYDvFk",
       // 所在省份
       province: "贵州",
       // 所在城市
